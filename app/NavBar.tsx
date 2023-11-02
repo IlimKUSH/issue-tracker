68b65a10e953f7695c0cafe18454a41ff5f2ecb1
ilimKUSH <ilimKUSH>
2023-11-02 11:30:17 +0600

    added prisma and fixed navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,30 @@
+"use client";
 import React from 'react';
 import Link from "next/link";
 import {AiFillBug} from "react-icons/ai"
+import {usePathname} from "next/navigation";
+import cx from 'classnames';
 
 export const NavBar = () => {
+  const currentPath = usePathname()
+
   const links = [
-    {label: "Dashboard", value: "/"},
-    {label: "Issues", value: "/issues"}
+    {label: "Dashboard", href: "/"},
+    {label: "Issues", href: "/issues"}
   ]
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/"><AiFillBug/></Link>
       <ul className="flex space-x-6">
-        {links.map(({label, value}) =>
+        {links.map(({label, href}) =>
           <Link
             key={label}
-            href={value}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors">
+            href={href}
+            className={cx({
+              "text-zinc-900": href === currentPath,
+              "text-zinc-500 hover:text-zinc-800 transition-colors": true
+            })}>
             {label}
           </Link>
         )}
