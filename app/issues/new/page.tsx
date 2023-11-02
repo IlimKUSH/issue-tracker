f1cc319f393498c396870379faf9af76c376181b
ilimKUSH <ilimKUSH>
2023-11-02 17:56:36 +0600

    build new Issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+import {Button, TextArea, TextField} from "@radix-ui/themes";
+
+export default function Page() {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Button} from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button><Link href="/issues/new">New Issue</Link></Button>
     </div>
   );
 };
